338655ff5679ab67357538a9ea0a5e931bbbfdd6
Cong Wang <wangcong525@hotmail.com>
2024-08-26 11:01:53 -0400

    able to get all products

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -3,11 +3,11 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
+//get all products
 router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll({
-      include: [{model: Category}, {model: Tag}, {model: ProductTag}],
+      include: [{model: Category}, {model: Tag}],
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -16,6 +16,7 @@ router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
 });
+
 
 // get one product
 router.get('/:id', async (req, res) => {
