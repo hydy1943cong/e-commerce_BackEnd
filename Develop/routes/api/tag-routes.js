3427cd8b74e84de6731ca5fac4e326128ff35be1
Cong Wang <wangcong525@hotmail.com>
2024-08-26 14:31:13 -0400

    update,put, delete tags updated

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -35,16 +35,53 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+// router.post('/', (req, res) => {
+//   // create a new tag
+// });
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+// router.put('/:id', (req, res) => {
+//   // update a tag's name by its `id` value
+// });
+
+router.put('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagData[0]) {
+      res.status(404).json({ message: 'No tag with this id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-});
+// router.delete('/:id', (req, res) => {
+//   // delete on tag by its `id` value
+// });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  // delete one category by its `id` value
+  Tag.destroy ({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((deletedTag) => {
+    res.json(deletedTag);
+  })
+  .catch((err) => res.json(err));
 });
 
 module.exports = router;
